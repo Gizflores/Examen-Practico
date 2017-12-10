0ad3e30654453b0f2c9f7b221ecdce565739cfdf
Gizflores <diegooflores14@gmail.com>
2017-12-09 17:36:42 -0700

    Add files via upload

---
+++ Practica 2/EVA2_5_PICKER_VIEW/ViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ViewController.swift
+//  EVA2_5_PICKER_VIEW
+//
+//  Created by TEMPORAL2 on 19/10/17.
+//  Copyright © 2017 TEMPORAL2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBAction func ActivadorBoton(_ sender: Any) {
+        if (b1 && b2 && b3 == true) {
+            
+            let alert = UIAlertController(title: "Success", message: "Contraseña Correcta", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        if (b1 || b2 || b3 == false) {
+            let alert = UIAlertController(title: "Fail", message: "La contraseña es incorrecta", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    @IBOutlet weak var textlabel: UILabel!
+    var x = "1"
+    var y = "2"
+    var z = "3"
+    var b1 = false
+    var b2 = false
+    var b3 = false
+    let aDias = ["0","1","2","3","4","5","6","7","8","9"]
+    let aNumero1 = ["0","1","2","3","4","5","6","7","8","9"]
+    let aNumero2 = ["0","1","2","3","4","5","6","7","8","9"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (component == 0) {
+            return aDias.count
+        }
+        if (component == 2) {
+            return aNumero1.count
+        }
+        return aNumero2.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (component == 0) {
+            return aDias[row]
+        }
+        if (component == 1) {
+            return aNumero1[row]
+        }
+        return aNumero2[row]
+    }
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        
+        if (component == 0) {
+           textlabel.text = aDias[row]
+            
+            if (x == aDias[row]) {
+                
+                b1=true
+                textlabel.text = "Primer digito correcto"
+            
+        }
+            if(x != aDias[row]){
+                b1=false
+            }
+        }
+        if (component == 1) {
+            textlabel.text = aNumero1[row]
+            
+            if (y == aNumero1[row]) {
+                
+                b2=true
+                textlabel.text = "Segundo digito correcto"
+        }
+            if(y != aDias[row]){
+                b2=false
+            }
+        }
+       if (component == 2) {
+            textlabel.text = aNumero2[row]
+                
+            if (z == aNumero2[row]) {
+                    
+                b3=true
+                textlabel.text = "Tercer digito correcto"
+            
+        }
+            if(z != aDias[row]){
+                b3=false
+        }
+        
+            }
+        }
+
+}
